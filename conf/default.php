234ee3f07f90fdc79ce1db1ea2f632e36b995d47
tormec <dcstoyanov@gmail.com>
2020-03-23 00:37:13 +0100

    created onoff button in configuration manager

---
+++ conf/default.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * AcMenu plugin: an accordion menu for namespaces and relative pages.
+ *
+ * default.php: default metadata settings used in the Configuration Manager.
+ *
+ * @author Torpedo <dcstoyanov@gmail.com>
+ * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @package default_settings
+ */
+
+$conf["startop"] = 0;
---
+++ conf/metadata.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * AcMenu plugin: an accordion menu for namespaces and relative pages.
+ *
+ * metadata.php: kinds of metadata used in the Configuration Manager.
+ *
+ * @author Torpedo <dcstoyanov@gmail.com>
+ * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @package metadata
+ */
+
+// startop = (int) value of the checkbox:
+//     0 = (default) start page is sorted according to its heading
+//     1 = start page is not renamed and kept on top
+$meta["startop"] = array("onoff");
