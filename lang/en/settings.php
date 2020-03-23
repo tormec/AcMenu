1cfffdde7e5013afa5fd386b6750dfa9bf76d191
tormec <dcstoyanov@gmail.com>
2020-03-23 15:02:22 +0100

    created

---
+++ lang/en/settings.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * AcMenu plugin: an accordion menu for namespaces and relative pages.
+ *
+ * settings.php: strings used by the PyCode plugin in Configuration Manager.
+ *
+ * @author Torpedo <dcstoyanov@gmail.com>
+ * @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @package en_lang_settings
+ */
+
+$lang["startop"] = "Don't rename <code>start</code> page and keep it on top.";
